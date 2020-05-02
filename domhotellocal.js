72d26d36230246f8028c4cf92da63d7d34e80df8
Marguerite Blair <marguerite.blair@gmail.com>
2020-05-02 00:26:19 -0400

    dom hotel local storage

--- domhotellocal.js
+++ domhotellocal.js
@@ -2,12 +2,16 @@ document.getElementById("title").innerText = "WELCOME TO THE NEWEST BRANCH OF TH
 document.getElementById("slogan").innerText = "Where raspberry pi is complimentary at every meal!"
 
 let roomsAvailable = [313, 314, 315, 316, 317];
-let amenities = ["Spa", "Room Service", "24/7 Concierge", "Our Mercedes Program"];
 
-let dropDownRooms = "<select>"
+let dropDownRooms = "<select id = rooms>"
 for (let i = 0; i < roomsAvailable.length; i++) {
     dropDownRooms = dropDownRooms + `<option value = "${roomsAvailable[i]}">${roomsAvailable[i]}</option>`
 }
 dropDownRooms = dropDownRooms + "</select>"
 
 document.getElementById("rooms").innerHTML = dropDownRooms;
+
+function checkIn() {
+    let newVar = document.getElementById("rooms").value
+    localStorage.setItem("CheckedInRooms", newVar)
+}
