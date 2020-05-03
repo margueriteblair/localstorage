d02b87bcde63284ca5baaf8159eb4a32c9e6bb40
Marguerite Blair <marguerite.blair@gmail.com>
2020-05-03 19:12:25 -0400

    dates pushed to local storage

--- hoteldates.js
+++ hoteldates.js
@@ -1,13 +1,14 @@
-document.getElementById("header").innerText = "Welcome to the CareerDevs Hotel!"
-document.getElementById("slogan").innerText = "Code Now, Rest Never"
-document.getElementById("instructions").innerText = "Please input the dates you would like to stay and the room number."
-
-let roomsAvailable = [101, 102, 103, 104];
-
-let dropdown = "<select>"
-for (let i = 0; i < roomsAvailable.length; i++) {
-    dropdown = dropdown + `<option value = "${roomsAvailable[i]}">${roomsAvailable[i]}</option>`
-}
-    dropdown=dropdown + "</select>"
-    document.getElementById("dropdown").innerHTML = dropdown;
-    
+document.getElementById('hotelName').innerText = "Coder's Paradise";
+document.getElementById('hotelSlogan').innerText = 'Code Now, Rest Later!';
+​
+​
+function updateDates() {
+    localStorage.setItem('date1', document.getElementById('checkInDate').value); 
+    document.getElementById('checkIn').innerText = localStorage.getItem('date1'); 
+​
+    localStorage.setItem('date2', document.getElementById('checkOutDate').value);
+    document.getElementById('checkOut').innerText = localStorage.getItem('date2');
+} 
+​
+document.getElementById('checkIn').innerText = localStorage.getItem('date1');
+document.getElementById('checkOut').innerText = localStorage.getItem('date2');
